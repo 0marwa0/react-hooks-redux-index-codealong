850c17315f9cb82e18e7d080fbb5b5930fe5fb9e
marwa <marwajawad19@gmail.com>
2021-08-30 18:37:59 -0400

    maping over the todos

--- src/features/todos/TodoContainer.js
+++ src/features/todos/TodoContainer.js
@@ -1,7 +1,9 @@
 import React from "react";
 
 function TodoContainer() {
-  return <div>TodoContainer</div>;
+  const todos = useSelector((state) => state.todos.entities);
+  const todoList = todos.map((todo, index) => <Todo key={index} text={todo} />);
+    return <ul>{todoList}</ul>;
 }
 
 export default TodoContainer;
