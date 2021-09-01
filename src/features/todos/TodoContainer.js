2722f3cbfce6737c4bf4b8c5cce43e248f0b23bc
marwa <marwajawad19@gmail.com>
2021-09-01 06:35:15 -0400

    importing useSelector from redux

--- src/features/todos/TodoContainer.js
+++ src/features/todos/TodoContainer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {useSelector} from "react-redux"
 import Todo from "./Todo";
 function TodoContainer() {
   const todos = useSelector((state) => state.todos.entities);
